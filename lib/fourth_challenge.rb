7e5505a0467c3a5c05dc2f39072f0a80a2e6827a
Alexander Reyes Rodriguez <arayrods@gmail.com>
2020-05-18 02:05:27 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,15 +4,26 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        #your hashes here!
+      :hero_friends => [        {name: "Bolivio",
+         age: "15"
+         attitude: "worried"
+         },
+         {name: "Mercurio",
+          age: "17",
+          attitude: "hot-headed"}
+      
       ]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{name: "Steven", 
+         age: "30", 
+         attitude: "confused"},
+        {name: "Nurse", 
+         age: "44", 
+         attitude: "worried"}
         #your hashes here!
       ]
    }
